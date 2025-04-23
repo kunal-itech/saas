e3ef297150572eeae085b807f210613215ca897c
Kunal <kunal.gulati@itechnolabs.tech>
2025-04-23 17:04:10 +0530

    added the project

---
+++ src/cfehome/views.py
@@ -0,0 +1,22 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    context = {
+        "page_title": "My Page",
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": qs.count()
+
+    }
+    path = request.path
+    
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, context)
